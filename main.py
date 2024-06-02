9c91d4c8cf2de55cb45a872105a8418b79376be6
Simanta Sarob <simantasaha88@gmail.com>
2024-06-03 01:13:49 +0600

    clean code

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ print("for documentation type doc and enter.")
 def main():
     while True:
         command = input("Bank > ")
+        #print("\n")
         command_str(command)
         sleep(0.1)
         
@@ -32,21 +33,22 @@ def main():
         
         elif command == "login": # login and pass encrypt
             
-            logout(name='',password='') #added this line because of, if someone do login and use login command again, his/her previous login details willbe eraised.
+            logout(name='',password='') #added this line because of, if someone do login and use login command again, his/her previous login details will be eraised.
             
             name = input("name: ")
             plain_password  = input("password: ")
             password = hashlib.sha256(plain_password.encode()).hexdigest()
+            
             conn = sqlite3.connect('bank.db')
             cursor = conn.cursor()  
             cursor.execute("SELECT id FROM customer WHERE name = ? AND password = ?",(name,password,))
             value = cursor.fetchone()
             
             if value is not None:
-                print("loged in successful.")
+                print("Loged in successful.")
                 login(name,password)
             else:
-                print("name or password is not correct.")
+                print("Name or password is not correct.")
                 
             conn.close()
             
@@ -55,51 +57,60 @@ def main():
             name = ''
             password = ''
             logout(name,password)
-            print("logout done.")
+            print("Logout done.")
         
         elif command == "signup": #sign up
             name = input("your name: ")
-            dob = input("your date of birth: ")
-            amount = input("the amount of money you want to keep in the bank: ")
-            plain_password  = input("type your password: ")
-            plain_password_again  = input("type your password again: ")
+            dob = input("Your date of birth (YYYY-MM-DD): ")
+            amount = input("The amount of money you want to keep in the bank: ")
+            plain_password  = input("Type your password: ")
+            plain_password_again  = input("Type your password again: ")
+            
             if plain_password == plain_password_again:
-                password = hashlib.sha256(plain_password.encode()).hexdigest()
+                password = hashlib.sha256(plain_password.encode()).hexdigest() 
+                
             else:
-                print("try again with signup command.")
+                print("Try again later with 'signup' command.")
+                
             signup(name, password, dob, amount)
             
         elif command == "doc": #documentation
             doc()
         
         elif command =="deposite": #deposite
-            given_amount = input("amount: ")
+            given_amount = input("Amount: ")
             deposite(given_amount)
 
         elif command =="withdrew": #withdrew
-            given_amount = input("amount: ")
+            given_amount = input("Amount: ")
             withdrew(given_amount)
         
         elif command == "id":
-            name = input("name: ")
+            name = input("Name: ")
             id_num(name) 
         
         elif command == "login details":
+            
             file = open("name.txt",'r')
             name = file.read()
+            
             if name =="":
-                print("name: Null")
+                print("Name: Null")    
             else:
-                print(F"name: {name}")
+                print(F"Name: {name}")
+                
             id_num(name)
         
         elif command == "send money":
             sender_id_txt = input("YOUR ID: ")
             reciver_id_txt = input("RECIVER ID: ")
-            amount_send_txt = input("amount: ")
+            amount_send_txt = input("The amount you want to sent: ")
+            
             send_money(sender_id_txt, reciver_id_txt, amount_send_txt)
             
         else:
             print("not valid command. use 'doc' command to see all the valid command/s.")
+            
+        #print("\n")
         
 main()
