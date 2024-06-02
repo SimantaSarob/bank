16c896e85929fbb76b03fb82add8ffefc283f7b3
Simanta Sarob <simantasaha88@gmail.com>
2024-06-03 00:37:29 +0600

    Bug fixs. Shoing all ids with the same name.

--- id_num_fun.py
+++ id_num_fun.py
@@ -12,10 +12,23 @@ def id_num(name):
     else:
         if result[0]>0:
             cursor.execute("SELECT id FROM customer WHERE name = ? ",(name,))
-            value = cursor.fetchone()
-            print(f"id: {value[0]}")
+            values = cursor.fetchall()
+            
+            ids = []    # collectinh multiple values in tauple
+            for value in values:
+                ids.append(value[0])
+                
+            size = len(ids)     # finding the size of the tauple for while loop's break condition.
+            start = 0
+            
+            while True:
+                print(f"id: {ids[start]}")  # printing the data.
+                start = start + 1
+                if start == size:
+                    break
+                
             conn.close()
         
         else:
-            print(f"no one found with provided name:{name}")
+            print(f"no one found with provided name:'{name}'")
             conn.close()
