ec7b957206506db48791d52b6187d475735420f1
Simanta Sarob <simantasaha88@gmail.com>
2024-06-04 17:01:51 +0600

    ...

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ import sqlite3
 conn = sqlite3.connect("bank.db")
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT, dob DATE, amount TEXT);")
+cursor.execute("CREATE TABLE IF NOT EXISTS customer (id INTEGER, name TEXT, password TEXT, dob DATETIME, amount INTEGER, account_creating_date DATETIME DEFAULT 'none', account_creating_time	DATETIME DEFAULT 'none', PRIMARY KEY(id AUTOINCREMENT))")
 conn.commit()
-cursor.execute("CREATE TABLE IF NOT EXISTS history (command_number INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, command TEXT, execution_time NUMERIC)")
 
+cursor.execute("CREATE TABLE IF NOT EXISTS history (command_number INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, command TEXT, execution_time NUMERIC)")
 conn.commit()
+
 conn.close()
 
 file_name = open("name.txt",'w')
 file_name.write('')
+
 file_password = open("password.txt", 'w')
-file_password.write
+file_password.write('')
+
 print("setup done.")
