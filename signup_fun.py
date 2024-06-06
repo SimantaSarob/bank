425177a53c410f19a6372e46ace37ff6efd5221e
Simanta Sarob <simantasaha88@gmail.com>
2024-06-06 22:40:26 +0600

    added a file name

--- signup_fun.py
+++ signup_fun.py
@@ -1,6 +1,8 @@
+import select
 import sqlite3
 from datetime import datetime
 from time import strftime
+from token import NAME
 
 ####################################
 #                                  #
@@ -15,15 +17,39 @@ def signup(name, password, dob, amount, email):
     amount_sql = float(amount)
     email_sql = email
     
+    
+    
     time = datetime.now()
     date_now = time.date()
     time_now = time.time()
     
     conn = sqlite3.connect('bank.db')
     cursor = conn.cursor()  
-    cursor.execute("INSERT INTO customer (name,password,dob,amount,account_creating_date,account_creating_time,email) VALUES(?,?,?,?,?,?)",(name_sql,password_sql, dob_sql, amount_sql, date_now.strftime("%Y-%m-%d"),time_now.strftime("%H:%M:%S"),email_sql))
     
+    cursor.execute("INSERT INTO customer (name,password,dob,amount,account_creating_date,account_creating_time,email) VALUES(?,?,?,?,?,?,?)", ( name_sql , password_sql , dob_sql , amount_sql , date_now.strftime("%Y-%m-%d") , time_now.strftime("%H:%M:%S") , email_sql ,))
     conn.commit()
+    
+    cursor.execute("SELECT id FROM customer WHERE name = ? and email = ? values(?,?)", (name_sql,email_sql,))
+    alloted_id = cursor.fetchone()
+    
+    users_alloted_id = alloted_id[0] # for easy development
+    '''
+    
+    
+    # creating users personal table's name_variable where we can track him. 
+    users_table_name = f"name:{name_sql} id:{users_alloted_id[0]}"
+    '''
     conn.close()
     
-    print("ok. done.")
+    text = f'''
+    ----------------------------------------------------------------------------------
+    #   YOU NEED TO REMEMBER THIS DATA:
+    #
+    #   NAME     : {name}
+    #   ID       : {users_alloted_id}
+    #   PASSWORD : you know that.
+    #   
+    #   this data will halp you to interact with your account.
+    ----------------------------------------------------------------------------------
+    '''
+    print(text)
