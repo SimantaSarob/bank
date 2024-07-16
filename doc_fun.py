da1dc9b4a03a54745d83ef28fd12ada219f1bfca
Simanta Sarob <simantasaha88@gmail.com>
2024-07-17 00:10:54 +0600

    Proper Documentation added

--- doc_fun.py
+++ doc_fun.py
@@ -1,17 +1,19 @@
 def doc():
     print('''
-          hello. its a demo bank which is made and maintain by Simanta Saha Sarob. 
-          
-          login > log in your account. give your account name, and password.
-          logout > log out from your profile.
-          exit > exit.
-          doc > for documentation.
-          total > will give you the amount of money you store in your bank.
-          deposite > to deposite your money.
-          withdrew > withdrew your money.
-          id >
-          login details >
-          send money >
-          
+      #*****************************************************************************************
+      #  Hello. It's a demo banking system which is made and maintain by Simanta Saha Sarob.   #
+      #*****************************************************************************************
+      #  login > Log in your account. Give your account name, ID and password.                 #
+      #  logout > Log out from your profile.                                                   #
+      #  exit > Will exit the code.                                                            #
+      #  doc > For documentation.                                                              #
+      #  total > Will give you the amount of money you stored in your bank account.            #
+      #  deposite > To deposite your money.                                                    #
+      #  withdrew > Withdrew your money.                                                       #
+      #  id > Find someones name through different informations.                               #
+      #  login details > Show everything about account.                                        #
+      #  send money > Send money to Different users. (Need to know recievers ID number.)       #
+      #*****************************************************************************************
           
           ''')
+doc()
