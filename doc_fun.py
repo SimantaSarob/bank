65e3122f5b17004084a8e7d25e6216da93e46174
Simanta Sarob <simantasaha88@gmail.com>
2024-07-17 20:32:08 +0600

    changed the output format

--- doc_fun.py
+++ doc_fun.py
@@ -1,18 +1,19 @@
 def doc():
     print('''
-      #*****************************************************************************************
-      #  Hello. It's a demo banking system which is made and maintain by Simanta Saha Sarob.   #
-      #*****************************************************************************************
-      #  login > Log in your account. Give your account name, ID and password.                 #
-      #  logout > Log out from your profile.                                                   #
-      #  exit > Will exit the code.                                                            #
-      #  doc > For documentation.                                                              #
-      #  total > Will give you the amount of money you stored in your bank account.            #
-      #  deposite > To deposite your money.                                                    #
-      #  withdrew > Withdrew your money.                                                       #
-      #  id > Find someones name through different informations.                               #
-      #  login details > Show everything about account.                                        #
-      #  send money > Send money to Different users. (Need to know recievers ID number.)       #
-      #*****************************************************************************************
-          
-          ''')
++------------------------------------------------------------------------------------+
+| Hello! It's a demo banking system which is made and maintain by Simanta Saha Sarob.|
++---------------+--------------------------------------------------------------------+
+| Command       | Discripytion                                                       |
++---------------+--------------------------------------------------------------------+
+| login         | Log in your account. Give your account name, ID and password.      |
+| logout        | Log out from your profile.                                         |
+| exit          | Will exit the code.                                                |
+| doc           | For documentation.                                                 |
+| total         | Will give you the amount of money you stored in your bank account. |
+| deposite      | To deposite your money.                                            |
+| withdrew      | Withdrew your money.                                               |
+| id            | Find someones name through different informations.                 |
+| login details | Show everything about account.                                     |
+| send money    | Send money to Different users. (Need to know recievers ID number.) |
++---------------+--------------------------------------------------------------------+
+''')
