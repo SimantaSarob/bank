e8e33f9bb0845040e74d3d3c1e7e908a67a5a791
Simanta Sarob <simantasaha88@gmail.com>
2024-07-20 15:54:56 +0600

    polished | improved

--- user_table_fun.py
+++ user_table_fun.py
@@ -15,6 +15,6 @@ def create_users_table(users_table_name):
     conn = sqlite3.connect("bank.db")
     cursor = conn.cursor()
     
-    cursor.execute(f"CREATE TABLE {users_table_name} (event_no INTEGER, date DATETIME, time	DATETIME, amount	INTEGER,  command	TEXT, t_amount	INTEGER , sent_amount	INTEGER ,sent_id integer, name	TEXT , deposit_amount	INTEGER , withdrew_amount	INTEGER , new_amount integer, PRIMARY KEY(event_no AUTOINCREMENT))")
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {users_table_name} (event_no INTEGER, date DATETIME, time	DATETIME, amount	INTEGER,  command	TEXT, t_amount	INTEGER , sent_amount	INTEGER ,sent_id integer, name	TEXT , deposit_amount	INTEGER , withdrew_amount	INTEGER , new_amount integer, PRIMARY KEY(event_no AUTOINCREMENT))")
     conn.commit()
     conn.close()
