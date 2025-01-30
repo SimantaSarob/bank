b118fd6fd26a32864bd5dac114d66d4c15bd2ef1
Sarob <78264270+SimantaSarob@users.noreply.github.com>
2025-01-30 16:32:05 +0600

    regular backup and not tested.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 First run the `setup.py` file.
 
+this file will ask you some question for creating admin account. It is very important. So be careful. just read what they are asking and give answers carefully. 
+
 Then run the `main.py` file.
--- admin_.py
+++ admin_.py
@@ -2,20 +2,24 @@ import time
 from doc_fun import admin_doc
 import sqlite3
 from clear_fun import clear
+import os
+
+conn = sqlite3.connect("bank.db")
+cur = conn.cursor()
 
 def admin():
     while True:
-        command = input("Admin : ")
+        command = input("Bank : Admin > ")
         time.sleep(0.01)
-        if command == "doc":
+        if command == "doc" or "help":
             admin_doc()
             
         elif command == "customer":
             conn = sqlite3.connect("bank.db")
             cur = conn.cursor()
-            
             cur.execute("SELECT * FROM customer")
             customer_data = cur.fetchall()
+            conn.close()
             
             for data in customer_data:
                 print(data)
@@ -26,5 +30,35 @@ def admin():
         elif command == "clear":
             clear()
         
+        elif command == "history":
+            conn = sqlite3.connect("bank.db")
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM history")
+            history_data = cur.fetchall()
+            conn.close()
+            for data in history_data:
+                print(data)
+        
+        elif command == "system":
+            print("\nSystem Level Access Grunted.\nCarefull what you do.\nUse 'exit' to go back to Admin access.\n")
+            while True:
+                cmd = input("Bank : Admin : System > ")
+                os.system(cmd)
+                if cmd == "exit":
+                    break
+                
+        elif command == "db":
+            print("\nBank Database Access Grunted.\nCarefull what you do.\nUse 'exit' to go back to Admin access.\n")
+            conn = sqlite3.connect("bank.db")
+            cur = conn.cursor()
+            while True: 
+                sql = input("Bank : Admin : bank.db (sql) > ")
+                if sql == "exit":
+                    break
+                cur.execute(sql)
+                all_data = cur.fetchall()
+                for data in all_data:
+                    print(data)
+        
         else:
-            print("Not A valid Admin command.")
+            print("Not A valid Admin Command.")
